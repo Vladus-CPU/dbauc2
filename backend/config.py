d96cf0ab042686b37aa6ed63253bbb6c2d89dc24
Качановський Владислав bc2025-2 <kacanovskijvlad@gmail.com>
2025-10-29 22:03:51 +0200

    fix: update database configuration to use os.getenv and ensure dotenv is loaded

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,10 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 DB_CONFIG = {
-    'host': os.environ.get('DB_HOST', 'localhost'),
-    'user': os.environ.get('DB_USER', 'root'),
-    'password': os.environ.get('DB_PASSWORD', ''),
-    'database': os.environ.get('DB_NAME', 'auction'),
-    'port': int(os.environ.get('DB_PORT', '3306')),
+    'host': os.getenv('DB_HOST', ''),
+    'user': os.getenv('DB_USER', ''),
+    'password': os.getenv('DB_PASSWORD', ''),
+    'database': os.getenv('DB_NAME', ''),
+    'port': int(os.getenv('DB_PORT', '')),
 }
