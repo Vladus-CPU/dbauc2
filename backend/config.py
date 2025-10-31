3d66e216bae5660f60f82d8f7ed5f0fb05ccaa1b
Качановський Владислав bc2025-2 <kacanovskijvlad@gmail.com>
2025-10-31 14:26:43 +0200

    fix: resolve merge conflict in config.py

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,5 @@
 import os
 from dotenv import load_dotenv
-<<<<<<< HEAD
-
-=======
->>>>>>> a4bfcf4425373ef479ca8fe1bb8bdf555b55d1fc
 load_dotenv()
 
 DB_CONFIG = {
@@ -12,8 +8,4 @@ DB_CONFIG = {
     'password': os.getenv('DB_PASSWORD'),
     'database': os.getenv('DB_NAME'),
     'port': int(os.getenv('DB_PORT')),
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a4bfcf4425373ef479ca8fe1bb8bdf555b55d1fc
